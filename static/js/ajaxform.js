9656f60bf23b933b41dc2b5f0f5c6ca61e082b1b
Sauli Kylmänen <62215427+saukkko@users.noreply.github.com>
2022-02-18 23:35:02 +0200

    forms update - added content to ajaxform.html - created ajaxform.js - message limit is now 500 - minor adjustments to script and style loading

---
+++ static/js/ajaxform.js
@@ -0,0 +1,34 @@
+"use strict";
+
+window.onload = () => {
+  const form = document.forms.item(0);
+  form.addEventListener("submit", (ev) => {
+    ev.preventDefault();
+
+    const name = form.name.value;
+    const country = form.country.value;
+    const message = form.message.value;
+
+    const formData = {
+      name: name,
+      country: country,
+      message: message,
+    };
+
+    const payload = Object.entries(formData)
+      .map((x) => x.join("="))
+      .join("&");
+
+    fetch("/post", {
+      body: payload,
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      method: "POST",
+      redirect: "manual",
+    })
+      .then(async (res) => {
+        if (typeof res !== "undefined" && res instanceof Response)
+          await import("/js/makeTable.js").then((x) => console.log(x));
+      })
+      .catch((err) => console.error(err));
+  });
+};
--- static/js/counter.js
+++ static/js/counter.js
@@ -1,10 +1,14 @@
+"use strict";
 const textarea = document.getElementById("messagefield");
 const counter = document.getElementById("counter");
+const MAX = 500;
+
+counter.innerText = `0/${MAX}`;
 
 const handleChange = (e) => {
   const len = e.target.value.length;
-  counter.innerText = `${len}/2000`;
-  if (len > 2000) textarea.value = e.target.value.slice(0, 2000);
+  counter.innerText = `${len}/${MAX}`;
+  if (len > MAX) textarea.value = e.target.value.slice(0, MAX);
 };
 
 textarea.oninput = handleChange;
